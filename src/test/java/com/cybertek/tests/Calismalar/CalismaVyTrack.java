7ebdfa71a8dde8696918fef1682a1cdd766c5d81
Cengiz-66 <cenkiskimya@gmail.com>
2020-05-13 12:53:18 +0200

    update CalismaVyTrack.java

--- src/test/java/com/cybertek/tests/Calismalar/CalismaVyTrack.java
+++ src/test/java/com/cybertek/tests/Calismalar/CalismaVyTrack.java
@@ -1,5 +1,7 @@
 package com.cybertek.tests.Calismalar;
 
+import com.cybertek.utilities.ConfigurationReader;
+import com.cybertek.utilities.Driver;
 import com.cybertek.utilities.JavaScript;
 import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
@@ -11,6 +13,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class CalismaVyTrack {
@@ -21,11 +24,15 @@ public class CalismaVyTrack {
 
     @BeforeMethod
     public void setUP() {
-        driver = WebDriverFactory.getDriver("chrome");
+        driver = Driver.get();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 20);
         actions = new Actions(driver);
+        driver.get(ConfigurationReader.get("url"));
+        driver.findElement(By.cssSelector("#prependedInput")).sendKeys(ConfigurationReader.get("driver_username"));
+        driver.findElement(By.id("prependedInput2")).sendKeys(ConfigurationReader.get("driver_password"));
+        driver.findElement(By.id("_submit")).click();
     }
 
     @AfterMethod
@@ -37,15 +44,11 @@ public class CalismaVyTrack {
     @Test
     public void test1_DragDrop() throws InterruptedException {
 
-        driver.get("https://qa2.vytrack.com/user/login");
-        driver.findElement(By.cssSelector("#prependedInput")).sendKeys("user11");
-        driver.findElement(By.id("prependedInput2")).sendKeys("UserUser123");
-        driver.findElement(By.id("_submit")).click();
         WebElement loaderFrame = driver.findElement(By.cssSelector("[class=\"loader-frame\"]"));
         wait.until(ExpectedConditions.invisibilityOf(loaderFrame));
 
         //go to fleet and vehicles and click
-        WebElement fleet = driver.findElement(By.xpath("(//span[contains(text(),'Fleet')])[1]"));
+        WebElement fleet = driver.findElement(By.cssSelector(".title.title-level-1"));
         actions.moveToElement(fleet).perform();
         WebElement vehicles = driver.findElement(By.xpath("(//span[contains(text(),'Vehicles')])[1]"));
         wait.until(ExpectedConditions.visibilityOf(vehicles));
@@ -71,6 +74,57 @@ public class CalismaVyTrack {
         driver.findElement(By.name("oro_calendar_event_form[organizerEmail]")).sendKeys("a@gmail.com");
         Thread.sleep(2000);
         driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+
+
+    }
+
+    @Test
+    public void test2() {
+        WebElement loaderFrame = driver.findElement(By.cssSelector("[class=\"loader-frame\"]"));
+        wait.until(ExpectedConditions.invisibilityOf(loaderFrame));
+
+        WebElement fleet = driver.findElement(By.cssSelector(".title.title-level-1"));
+        actions.moveToElement(fleet).perform();
+        WebElement costs = driver.findElement(By.xpath("(//span[@class=\"title title-level-2\"])[3]"));
+        wait.until(ExpectedConditions.visibilityOf(costs));
+        actions.moveToElement(costs).click().perform();
+        loaderFrame=driver.findElement(By.xpath("(//*[@class=\"loader-frame\"])[2]"));
+        wait.until(ExpectedConditions.invisibilityOf(loaderFrame));
+        WebElement createCosts = driver.findElement(By.cssSelector("[title=\"Create Vehicle Costs\"]"));
+        actions.moveToElement(createCosts).click().perform();
+        wait.until(ExpectedConditions.invisibilityOf(loaderFrame));
+        WebElement clickType = driver.findElement(By.cssSelector("[class=\"select2-chosen\"]"));
+        wait.until(ExpectedConditions.elementToBeClickable(clickType));
+        actions.moveToElement(clickType).click().perform();
+        List<WebElement> costType = driver.findElements(By.cssSelector("[class=\"select2-result-label\"]"));
+
+        for (WebElement type : costType) {
+            if(type.getText().contains("Tax Roll")) {
+                actions.moveToElement(type).click().perform();
+                break;
+            }
+        }
+
+        driver.findElement(By.cssSelector("[data-name=\"field__total-price\"]")).sendKeys("100");
+        WebElement date = driver.findElement(By.cssSelector("[placeholder=\"Choose a date\"]"));
+        actions.moveToElement(date).click().perform();
+        driver.findElement(By.cssSelector("[data-handler=\"today\"]")).click();
+        driver.findElement(By.cssSelector("[data-name=\"field__cost-descriptions\"]")).sendKeys("Tax Roll");
+        WebElement saveClose = driver.findElement(By.cssSelector("[type=\"submit\"]"));
+        actions.moveToElement(saveClose).click().perform();
+        actions.moveToElement(fleet).perform();
+        wait.until(ExpectedConditions.visibilityOf(costs));
+        actions.moveToElement(costs).click().perform();
+        System.out.print(driver.findElement(By.xpath("(//tbody)[1]/tr[1]/td[1]")).getText());
+        List<WebElement> elements = driver.findElements(By.xpath("(//tbody)[1]/tr/td[1]"));
+        for (int i = 1; i <=elements.size() ; i++) {
+            for (int j = 1; j <=2 ; j++) {
+                String path="(//tbody)[1]/tr["+i+"]/td["+j+"]";
+                WebElement cell=driver.findElement(By.xpath(path));
+                System.out.print(cell.getText()+"|");
+            }
+            System.out.println();
+        }
 
 
     }
